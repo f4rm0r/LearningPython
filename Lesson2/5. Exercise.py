8216c84722c19f53aa61dc303928f09d42e12f0f
Arch <anders.buja@gmail.com>
2022-06-03 19:07:24 +0200

    Update 5. Exercise.py

--- Lesson2/5. Exercise.py
+++ Lesson2/5. Exercise.py
@@ -5,6 +5,6 @@ myint = 20
 if mystring == "hello":
     print("String: %s" % mystring)
 if isinstance(myfloat, float) and myfloat == 10.0:
-    print("float: %f" % myfloat)
+    print("Float: %f" % myfloat)
 if isinstance(myint, int) and myint == 20:
     print("Integer: %d" % myint)
