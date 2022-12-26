42fad723ef34bfc215f10d31f85a10e0c29147c6
F4rm0r <anders.buja@gmail.com>
2022-12-26 01:45:05 +0100

    lesson3

---
+++ Round-2/Lesson3/simple_tests.py
@@ -0,0 +1,20 @@
+# Trying out strings.
+motto = "Facebook's old motto was 'move fast and break things'."
+print(motto)
+
+motto = 'Facebook\'s old motto was "move fast and break things".'
+print(motto)
+print("----------------------------------")
+
+# The \ escapes the function of ' in this case.
+
+integer_test = int(3.5)
+print(integer_test)
+
+integer_test = float(3.5)
+print(integer_test)
+
+# Just adding some flavor text in the shape of comments.
+#" going to try to type out somehting using only variables."
+
+print(motto + str(integer_test))
